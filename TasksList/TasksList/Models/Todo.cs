fb2a915056b26699860f470312ae86193e98bc7c
Luay <luayghazwan@yahoo.com>
2018-01-18 23:03:53 -0800

    List and Todo models created

---
+++ TasksList/TasksList/Models/Todo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TasksList.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public bool Completed { get; set; }
+        public int ListId { get; set; }
+    }
+}
