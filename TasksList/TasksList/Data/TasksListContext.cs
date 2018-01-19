b15f818ed296448ee45bf427f15b8fb9ba43cea7
Luay <luayghazwan@yahoo.com>
2018-01-18 23:04:07 -0800

    Task lists context created

---
+++ TasksList/TasksList/Data/TasksListContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using TasksList.Models;
+
+namespace TasksList.Data
+{
+    public class TasksListContext : DbContext
+    {
+        public TasksListContext(DbContextOptions<TasksListContext> options) : base(options) { }
+
+        internal DbSet<Todo> Tasks { get; set; }
+        internal DbSet<List> Lists { get; set; }
+    }
+}
